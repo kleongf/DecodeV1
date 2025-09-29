18b7a28504393c0c444726f47e67496a59a6539f
Kevin Fong <kf53307@pausd.us>
2025-09-29 13:57:37 -0700

    15 ball auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/GoodTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/GoodTeleop.java
@@ -10,10 +10,12 @@ import com.pedropathing.pathgen.Point;
 import com.pedropathing.pathgen.Vector;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.robot.robots.TeleopRobot;
 import org.firstinspires.ftc.teamcode.robot.robots.TeleopRobotV1;
 import org.firstinspires.ftc.teamcode.util.fsm.StateMachine;
+import org.firstinspires.ftc.teamcode.util.hardware.Drivetrain;
 import org.firstinspires.ftc.teamcode.util.hardware.SmartGamepad;
 import org.firstinspires.ftc.teamcode.util.misc.SOTM2;
 import org.firstinspires.ftc.teamcode.util.misc.Target;
@@ -25,20 +27,17 @@ import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 import java.util.HashMap;
 import java.util.Objects;
 
-@TeleOp(name="first good teleop GOOD?")
+@TeleOp(name="first good teleop: blue")
 public class GoodTeleop extends OpMode {
-    // TODO; future enhancements include:
-    // shooting automatically when in the tape and the wheels are moving at a tolerable speed and azimuth
-    // auto driving to setpoints (a consistent shooting setpoint AND human player)
-    // auto driving to the closest point in the tape, maintaining heading
-    // limelight relocalization
-
     private int state = 1;
-    private VoltageCompFollower follower;
+    private boolean isAutoDriving = false;
+    private Drivetrain drivetrain;
     private double longitudinalSpeed = 0.5, lateralSpeed = 0.5, rotationSpeed = 0.5;
     private TeleopRobotV1 robot;
     private final Pose startPose = new Pose(56, 6, Math.toRadians(180));
     private final Pose goalPose = new Pose(9, 132, Math.toRadians(45));
+    private final Pose shootPoseClose = new Pose(60, 84, Math.toRadians(180));
+    private final Pose shootPoseFar = new Pose(56, 8, Math.toRadians(180));
     private SmartGamepad gp1;
     private SmartGamepad gp2;
     private SOTM2 sotm2;
@@ -46,8 +45,8 @@ public class GoodTeleop extends OpMode {
 
     @Override
     public void init() {
-        follower = new VoltageCompFollower(hardwareMap, FConstants.class, LConstants.class);
-        follower.setStartingPose(startPose);
+        drivetrain = new Drivetrain(hardwareMap);
+        drivetrain.setStartingPose(startPose);
 
         robot = new TeleopRobotV1(hardwareMap);
 
@@ -78,7 +77,41 @@ public class GoodTeleop extends OpMode {
             Objects.requireNonNull(stateMap.get(Math.floorMod(state, 3))).start();
         }
 
-        double[] values = sotm2.calculateAzimuthThetaVelocity(follower.getPose(), new Vector());
+        if (gp1.xPressed()) {
+            PathChain driveClose = drivetrain.follower.pathBuilder()
+                    .addPath(
+                            new Path(
+                                    new BezierLine(
+                                            new Point(drivetrain.follower.getPose()),
+                                            new Point(shootPoseClose)
+                                    )
+                            )
+                    )
+                    .setLinearHeadingInterpolation(drivetrain.follower.getPose().getHeading(), shootPoseClose.getHeading())
+                    .build();
+            isAutoDriving = true;
+            drivetrain.follower.breakFollowing();
+            drivetrain.follower.followPath(driveClose, true);
+        }
+
+        if (gp1.yPressed()) {
+            PathChain driveFar = drivetrain.follower.pathBuilder()
+                    .addPath(
+                            new Path(
+                                    new BezierLine(
+                                            new Point(drivetrain.follower.getPose()),
+                                            new Point(shootPoseFar)
+                                    )
+                            )
+                    )
+                    .setLinearHeadingInterpolation(drivetrain.follower.getPose().getHeading(), shootPoseFar.getHeading())
+                    .build();
+            isAutoDriving = true;
+            drivetrain.follower.breakFollowing();
+            drivetrain.follower.followPath(driveFar, true);
+        }
+
+        double[] values = sotm2.calculateAzimuthThetaVelocity(drivetrain.follower.getPose(), drivetrain.follower.getVelocity());
 
         robot.turret.setTarget(values[0]);
         robot.shooter.setShooterPitch(values[1]);
@@ -88,13 +121,18 @@ public class GoodTeleop extends OpMode {
         telemetry.addData("velocity", values[2]);
         telemetry.addData("current velocity", robot.shooter.getCurrentVelocity());
 
-        follower.setTeleOpMovementVectors(
-                    gp1.getLeftStickY() * longitudinalSpeed,
-                    gp1.getLeftStickX() * lateralSpeed,
-                    gp1.getRightStickX() * rotationSpeed
-        );
+        if (isAutoDriving) {
+            if (!drivetrain.follower.isBusy()) {
+                isAutoDriving = false;
+                drivetrain.follower.breakFollowing();
+            }
+        } else {
+            drivetrain.setMovementVectors(gp1.getLeftStickY()*longitudinalSpeed,
+                    gp1.getLeftStickX()*lateralSpeed,
+                    gp1.getRightStickX()*rotationSpeed);
+        }
 
-        follower.update();
+        drivetrain.update();
         robot.update();
         telemetry.update();
     }
@@ -104,7 +142,6 @@ public class GoodTeleop extends OpMode {
         // TODO: In the future, initPositions() should go here so we don't move on init
         robot.initPositions();
         robot.shooter.setShooterOn(true);
-        follower.startTeleopDrive();
         robot.start();
     }
 }
