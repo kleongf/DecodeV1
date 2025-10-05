5e8862ad63d7cb84def253697888ab8c3a0d369d
lawrencezhao <chubbylarry@gmail.com>
2025-10-04 20:36:20 -0700

    coded 18 auto, can add cycles more by cycling more to the gate. no park yet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/BlueAutoCloseV2.java
@@ -0,0 +1,263 @@
+package org.firstinspires.ftc.teamcode.opmode.autonomous;
+
+import static java.lang.Thread.sleep;
+
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Vector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+import org.firstinspires.ftc.teamcode.robot.robots.AutonomousRobot;
+import org.firstinspires.ftc.teamcode.util.fsm.State;
+import org.firstinspires.ftc.teamcode.util.fsm.StateMachine;
+import org.firstinspires.ftc.teamcode.util.fsm.Transition;
+import org.firstinspires.ftc.teamcode.util.misc.SOTM2;
+import org.firstinspires.ftc.teamcode.util.misc.VoltageCompFollower;
+
+@Autonomous(name="blue auto close v2 - 18+, cycle from gate")
+public class BlueAutoCloseV2 extends OpMode {
+    public static final String END_POSE_KEY = "END_POSE";
+    private VoltageCompFollower follower;
+    private StateMachine stateMachine;
+    private AutonomousRobot robot;
+    private SOTM2 sotm2;
+    private final Pose startPose = new Pose(30, 137, Math.toRadians(270));
+    private final Pose shootPoseNear = new Pose(49, 85, Math.toRadians(180));
+    private final Pose shootPoseFar = new Pose(60, 22, Math.toRadians(180));
+
+    private final Pose goalPose = new Pose(9, 132, Math.toRadians(45));
+
+    private PathChain shootPreload, intakeSecond, shootSecond, openGate, shootGateCycle, intakeFirst, shootFirst, intakeThird, shootThird, intakeHuman, shootHuman;
+    public void buildPaths() {
+        shootPreload = follower.pathBuilder()
+                .addPath(
+                        new BezierLine(new Pose(30, 136.7), new Pose(49, 85))
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(250))
+                .build();
+        intakeSecond = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(49, 85),
+                                new Pose(53, 65),
+                                new Pose(22, 60)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(250), Math.toRadians(180))
+                .build();
+        shootSecond = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(22, 60),
+                                new Pose(53, 65),
+                                new Pose(49, 85)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(200))
+                .build();
+        openGate = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(49, 85),
+                                new Pose(34, 65),
+                                new Pose(10.5, 61)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(200), Math.toRadians(140))
+                .build();
+        openGate = follower.pathBuilder()
+                .addPath(
+                        // Path 6
+                        new BezierLine(new Pose(20, 60), new Pose(15, 70))
+                )
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+        shootGateCycle = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(10.5, 61),
+                                new Pose(42, 73),
+                                new Pose(49, 85)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(140), Math.toRadians(180))
+                .build();
+        intakeFirst = follower.pathBuilder()
+                .addPath(new BezierLine(new Pose(49, 85), new Pose(22, 84)))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+        shootFirst = follower.pathBuilder()
+                .addPath(new BezierLine(new Pose(22, 84), new Pose(49, 85)))
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(250))
+                .build();
+        intakeThird = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(49, 85),
+                                new Pose(53, 38),
+                                new Pose(22, 36)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(250), Math.toRadians(180))
+                .build();
+        shootThird = follower.pathBuilder()
+                .addPath(new BezierLine(new Pose(22, 36), new Pose(60, 22)))
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(180))
+                .build();
+        intakeHuman = follower.pathBuilder()
+                .addPath(new BezierLine(new Pose(60, 22), new Pose(10, 12)))
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(200))
+                //strafe slightly to make sure we get all the balls
+                .addPath(new BezierLine(new Pose(10, 12), new Pose(10, 8)))
+                .setConstantHeadingInterpolation(Math.toRadians(200))
+                .build();
+        shootHuman = follower.pathBuilder()
+                .addPath(new BezierLine(new Pose(10, 8), new Pose(60, 22)))
+                .setLinearHeadingInterpolation(Math.toRadians(200), Math.toRadians(200))
+                .build();
+    }
+
+    @Override
+    public void init() {
+        follower = new VoltageCompFollower(hardwareMap, FConstants.class, LConstants.class);
+        follower.setStartingPose(startPose);
+        robot = new AutonomousRobot(hardwareMap);
+        sotm2 = new SOTM2(goalPose);
+        buildPaths();
+
+        stateMachine = new StateMachine(
+                new State()
+                        .onEnter(() -> follower.followPath(shootPreload, true))
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.startShooting.start())
+                        .transition(new Transition(() -> robot.startShooting.isFinished())),
+                new State()
+                        .onEnter(() -> {
+                            robot.prepareIntake.start();
+                            follower.followPath(intakeSecond, true);
+                        })
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.prepareShooting.start())
+                        .transition(new Transition(() -> robot.prepareShooting.isFinished())),
+                new State()
+                        .onEnter(() -> follower.followPath(shootSecond, true))
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.startShooting.start())
+                        .transition(new Transition(() -> robot.startShooting.isFinished())),
+                new State()
+                        .onEnter(() -> {
+                            robot.prepareIntake.start();
+                            follower.followPath(openGate, true);//opens gate and intakes from same position hopefully
+                        })
+                        .transition(new Transition(() -> !follower.isBusy()))
+                        .maxTime(7000)
+                        .minTime(6000),//probably can wait a lot less for balls to come out, this is to be safe
+                new State()
+                        .onEnter(() -> robot.prepareShooting.start())
+                        .transition(new Transition(() -> robot.prepareShooting.isFinished())),
+                new State()
+                        .onEnter(() -> follower.followPath(shootGateCycle, true))
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.startShooting.start())
+                        .transition(new Transition(() -> robot.startShooting.isFinished())),
+                new State()
+                        .onEnter(() -> {
+                            robot.prepareIntake.start();
+                            follower.followPath(intakeFirst, true);
+                        })
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.prepareShooting.start())
+                        .transition(new Transition(() -> robot.prepareShooting.isFinished())),
+                new State()
+                        .onEnter(() -> follower.followPath(shootFirst, true))
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.startShooting.start())
+                        .transition(new Transition(() -> robot.startShooting.isFinished())),
+                new State()
+                        .onEnter(() -> {
+                            robot.prepareIntake.start();
+                            follower.followPath(intakeThird, true);
+                            double[] values2 = sotm2.calculateAzimuthThetaVelocity(shootPoseFar, new Vector());
+
+                            robot.turret.setTarget(values2[0]);
+                            robot.shooter.setShooterPitch(values2[1]);
+                            robot.shooter.setTargetVelocity(values2[2]);
+                        })
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.prepareShooting.start())
+                        .transition(new Transition(() -> robot.prepareShooting.isFinished())),
+                new State()
+                        .onEnter(() -> follower.followPath(shootThird, true))
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.startShooting.start())
+                        .transition(new Transition(() -> robot.startShooting.isFinished())),
+                new State()
+                        .onEnter(() -> {
+                            robot.prepareIntake.start();
+                            follower.followPath(intakeHuman, true);
+                        })
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.prepareShooting.start())
+                        .transition(new Transition(() -> robot.prepareShooting.isFinished())),
+                new State()
+                        .onEnter(() -> follower.followPath(shootHuman, true))
+                        .transition(new Transition(() -> !follower.isBusy())),
+                new State()
+                        .onEnter(() -> robot.startShooting.start())
+                        .transition(new Transition(() -> robot.startShooting.isFinished()))
+
+                //no park rn
+        );
+
+        Object endPose = blackboard.getOrDefault(END_POSE_KEY, new Pose(60, 22, Math.toRadians(200)));
+        blackboard.put(END_POSE_KEY, endPose);
+
+        try {
+            sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        robot.initPositions();
+    }
+    @Override
+    public void loop() {
+        stateMachine.update();
+        follower.update();
+        robot.update();
+    }
+
+    @Override
+    public void start() {
+        // constant values. shooter is never turned off after start.
+        double[] values = sotm2.calculateAzimuthThetaVelocity(shootPoseNear, new Vector());
+
+        robot.turret.setTarget(values[0]);
+        robot.shooter.setShooterPitch(values[1]);
+        robot.shooter.setTargetVelocity(values[2]);
+        robot.shooter.setShooterOn(true);
+
+        stateMachine.start();
+        robot.start();
+    }
+
+    // TODO: when we stop, save position to blackboard
+    @Override
+    public void stop() {
+        Object endPose = blackboard.getOrDefault(END_POSE_KEY, follower.getPose());
+        blackboard.put(END_POSE_KEY, endPose);
+    }
+}
