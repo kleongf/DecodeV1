0aee1a706c9e011616bfb746dff309165a6c3b79
Kevin Fong <kf53307@pausd.us>
2025-10-14 22:44:35 -0700

    prob right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class LConstants {
     static {
         PinpointConstants.forwardY = 2;
-        PinpointConstants.strafeX = -3.2;
+        PinpointConstants.strafeX = -3.7;
         PinpointConstants.distanceUnit = DistanceUnit.INCH;
         PinpointConstants.hardwareMapName = "pinpoint";
         PinpointConstants.useYawScalar = false;
