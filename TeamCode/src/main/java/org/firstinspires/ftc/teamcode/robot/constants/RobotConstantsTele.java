25fc11efe9a2f680c6f3b18645a3052bd549db62
amahl <amahlagrawal@gmail.com>
2025-10-25 14:19:54 -0700

    retuned pitch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants/RobotConstantsTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants/RobotConstantsTele.java
@@ -5,11 +5,11 @@ public class RobotConstantsTele {
     public static double INTAKE_PUSH_MID = 0.67;
     public static double INTAKE_DOWN = 0.73;
     public static double INTAKE_HIGH = 0.8;
-    public static double PITCH_I = Math.toRadians(25);
-    public static double PITCH_F = Math.toRadians(45);
-    public static double PITCH_SERVO_MIN = 0.42;
-    public static double PITCH_SERVO_I = 0.42;
-    public static double PITCH_SERVO_F = 0.9;
+    public static double PITCH_I = Math.toRadians(28);
+    public static double PITCH_F = Math.toRadians(50);
+    public static double PITCH_SERVO_MIN = 0.27;
+    public static double PITCH_SERVO_I = 0.27;
+    public static double PITCH_SERVO_F = 0.75;
     public static double LATCH_CLOSED = 0.58;
     public static double LATCH_OPEN = 0.2;
     public static double GATE_UP = 0.8;
