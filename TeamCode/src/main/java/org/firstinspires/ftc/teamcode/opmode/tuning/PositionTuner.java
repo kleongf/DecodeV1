6d1e6e4d522c406a9296010ea14d54babf70e68b
lawrencezhao <chubbylarry@gmail.com>
2025-12-02 10:25:45 -0800

    added shooting power to position tuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/PositionTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/PositionTuner.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.robot.constants.RobotConstants;
+import org.firstinspires.ftc.teamcode.util.controllers.FeedForwardController;
 
 @Config
 @TeleOp(name="Position Tuner")
@@ -16,14 +17,20 @@ public class PositionTuner extends OpMode {
     public Servo leftLatch;
     public Servo pitchServo;
     public DcMotorEx intakeMotor;
+    public DcMotorEx shooterMotor;
+    public DcMotorEx shooterMotor2;
     public static double latchTarget = RobotConstants.LATCH_CLOSED;
+    public static double shooterPower = 0;
     public static double intakePower = 0;
     public static double pitchTarget = RobotConstants.PITCH_SERVO_MIN;
+
+    private FeedForwardController controller;
 
     @Override
     public void init() {
         leftLatch = hardwareMap.get(Servo.class, "latchServo");
         pitchServo = hardwareMap.get(Servo.class, "pitchServo");
+
 
         leftLatch.setDirection(Servo.Direction.FORWARD);
         pitchServo.setDirection(Servo.Direction.FORWARD);
@@ -32,12 +39,26 @@ public class PositionTuner extends OpMode {
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        shooterMotor = hardwareMap.get(DcMotorEx.class, "shooterMotor");
+        shooterMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooterMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        shooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        shooterMotor2 = hardwareMap.get(DcMotorEx.class, "shooterMotor2");
+        shooterMotor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooterMotor2.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        shooterMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
     public void loop() {
+        shooterMotor.setPower(shooterPower);
+        shooterMotor2.setPower(shooterPower);
+
+
         intakeMotor.setPower(intakePower);
-       leftLatch.setPosition(latchTarget);
-       pitchServo.setPosition(pitchTarget);
+
+        leftLatch.setPosition(latchTarget);
+        pitchServo.setPosition(pitchTarget);
     }
 }
